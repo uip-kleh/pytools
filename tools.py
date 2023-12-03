8bece28fa586a5851cb904d0c17d81ba56ebc303
uip-kleh <h23ms416@hirosaki-u.ac.jp>
2023-12-03 16:21:41 +0900

    add iotools

--- tools.py
+++ tools.py
@@ -1,8 +1,33 @@
 import os
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+from PIL import Image
 
-class Tools:
+
+class IOTools:
     def __init__(self) -> None:
         pass
 
+    def read_csv(self, fname):
+        return pd.read_csv(fname)
+
+    def read_color_image(self, fname, asnumpy=False):
+        img = Image.open(fname)
+        if asnumpy: return np.array(img)
+        return img
+
+    def save_image(self, fname):
+        plt.savefig(fname, transparent=True)
+
+
+class Tools(IOTools):
+    def __init__(self) -> None:
+        super().__init__()
+
 if __name__ == '__main__':
     tools = Tools()
+    img = tools.read_color_image("test.jpg", asnumpy=True)
+    print(img.shape)
+
+
