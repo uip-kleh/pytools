3303e380e93b93a1fcfe53bb8260c3d17f62c854
uip-kleh <h23ms416@hirosaki-u.ac.jp>
2023-12-06 14:58:57 +0900

    add analysis tools

--- tools.py
+++ tools.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pylab as plt
 from PIL import Image
 import json
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -13,9 +14,13 @@ class BasicTools:
         pass
 
     def clear_figure(self):
-        plt.clear()
         plt.cla()
         plt.clf()
+        plt.close()
+
+    def save_image(self, fname):
+        plt.savefig(fname, transparent=True)
+        self.clear_figure()
 
 class IOTools(BasicTools):
     def __init__(self) -> None:
@@ -29,13 +34,43 @@ class IOTools(BasicTools):
         if asnumpy: return np.array(img)
         return img
 
-    def save_image(self, fname):
-        plt.savefig(fname, transparent=True)
-        self.clear_figure()
-
     def save_object(self, obj, fname):
         with open(fname, "w") as f:
             json.dump(obj, f, indent=2)
+
+class AnalysisTools(BasicTools):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def extract_objective(self, df: pd.DataFrame, column):
+        objective_df = df[column]
+        dropped_df = df.drop(column, axis=1)
+        return (objective_df, dropped_df)
+
+    def drop_columns(self, df: pd.DataFrame, columns):
+        return df.drop(columns, axis=1)
+
+    def has_nan(self, df: pd.DataFrame):
+        print(df.isnull().any())
+
+    def fill_nan_with_mode(self, df: pd.DataFrame):
+        return df.fillna(df.mode().iloc[0])
+
+    def label_encode(self, train_df: pd.DataFrame, test_df, columns):
+        for column in columns:
+            le = LabelEncoder()
+            le.fit(train_df[column])
+            train_df[column] = le.transform(train_df[column])
+            test_df[column] = le.transform(test_df[column])
+        return (train_df, test_df)
+
+    def onehot_encode(self, labels):
+        ohe = OneHotEncoder()
+        encoded_label = ohe.fit_transform(labels.reshape(1, -1))
+        return encoded_label
+
+    def split_data(self, data, labels):
+        return train_test_split(data, labels, stratify=labels, test_size=.2)
 
 class MLTools(BasicTools):
     def __init__(self) -> None:
@@ -85,7 +120,7 @@ class MLTools(BasicTools):
 
             return (train_gen, test_gen)
 
-class Tools(IOTools, MLTools):
+class Tools(IOTools, AnalysisTools, MLTools):
     def __init__(self) -> None:
         super().__init__()
 
